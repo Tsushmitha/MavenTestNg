4e5df5b3024a5f4ae9c38edc82f762fd748004e3
Tsushmitha <sushmit05796@gmail.com>
2020-01-24 11:28:26 +0530

    done

---
+++ src/test/java/jenkins/Jenkinsmaven.java
@@ -0,0 +1,17 @@
+package jenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Jenkinsmaven {
+
+	public void faceBook() {
+
+		
+	}
+
+}
